c96b1d463af04f9a30344ee6532f93ef31d9589d
eng-haider <31736386+eng-haider@users.noreply.github.com>
2026-02-22 01:00:54 +0300

    s

--- app/Http/Controllers/DemoRegisterController.php
+++ app/Http/Controllers/DemoRegisterController.php
@@ -27,9 +27,9 @@ class DemoRegisterController extends Controller
     {
         // --- Validation ---
         $validated = $request->validate([
-            'name'                  => ['required', 'string', 'max:255'],
-            'phone'                 => ['required', 'string', 'max:20'],
-            'password'              => ['required', 'string', 'min:6', 'confirmed'],
+            'user_name'                  => ['required', 'string', 'max:255'],
+            'user_phone'                 => ['required', 'string', 'max:20'],
+            'user_password'              => ['required', 'string', 'min:6', 'confirmed'],
         ]);
 
         try {
