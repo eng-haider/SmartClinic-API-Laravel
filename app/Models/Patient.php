41306bbd0558e9725be111dc1dfdc2ba9835f8c3
eng-haider <31736386+eng-haider@users.noreply.github.com>
2026-05-26 16:05:40 +0300

    Update Patient.php

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -257,7 +257,7 @@ class Patient extends Model
     public function scopeAllCasesPaid($query)
     {
         return $query->whereDoesntHave('cases', function ($q) {
-            $q->where('is_paid', false);
+            $q->where('is_paid', '!=', true);
         })->whereHas('cases'); // Must have at least one case
     }
 
